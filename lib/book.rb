5063e1c5ef70e0dd85e6e8d08260871bd244d310
devWRM <mcwen@comcast.net>
2020-06-19 12:47:18 -0400

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,23 @@
 
+class Book
+    attr_reader :title
+    attr_accessor :author, :genre
 
+    def initialize(title)
+        @title = title
+    end
+
+    def page_count=(page_count)
+         @page_count = page_count
+    end
+
+    def page_count
+        @page_count
+    end
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+
+
+end
