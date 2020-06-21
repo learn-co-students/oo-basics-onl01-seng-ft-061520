d9a0421a0c9dec88fe2d79200f7dae6c4410cfcf
Shirlen Detablan <slen.detablan@gmail.com>
2020-06-21 21:44:41 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,12 @@
-# Make your shoe class here!
+class Shoe 
+ attr_accessor :properties :color :size 
+ 
+  def initialize(brand)
+    @brand = brand
+  end
+ 
+  def brand=(shoe_brand)
+    @brand 
+    
+  end 
+end 
