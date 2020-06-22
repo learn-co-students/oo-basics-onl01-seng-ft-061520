74cf7cece0cd7eb9e53985ab7fbed4f84884b9a1
Samuel Grasse-Haroldsen <sgrasseharoldsen@gmail.com>
2020-06-22 13:58:37 -0700

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,15 @@
+class Book
+  def initialize(title) #,author)
+    @title = title
+    #@author = author
+  end
+  attr_accessor :title
+  attr_accessor :author
+  attr_accessor :page_count
+  attr_accessor :genre
 
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+end
 
