8bbf5c3d749778f745131309292f56c3b2f2377a
Jessica Pereira <jcpereira070@gmail.com>
2020-06-23 19:06:32 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,48 @@
 # Make your shoe class here!
+class Shoe
+  attr_accessor :brand, :color, :size, :material, :condition
+  def initialize(brand)
+    @brand = brand
+
+  end
+  def cobble
+    @condition = "new"
+    puts "Your shoe is as good as new!"
+
+  end
+  #   repaired=
+  #   new=
+  # end
+end
+
+# def title
+#   @title
+# end
+#
+# def author=(author)
+  # at author = author
+# end
+#
+# def author
+#   at author
+# end
+#
+# def page_count=(name)
+#   @page_count = name
+# end
+#
+# def page_count
+#   @page_count
+# end
+#
+# def genre=(genre)
+#   @genre = genre
+# end
+#
+# def genre
+#   @genre
+# end
+#
+# def turn_page
+#   puts "Flipping the page...wow, you read fast!"
+# end
